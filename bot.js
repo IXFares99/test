3ed024411be09f7662b3c7c8db1e4d736ce202ea
IXFares99 <52209189+IXFares99@users.noreply.github.com>
2019-06-25 18:22:54 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on("ready", () => {
 let channel =     client.channels.get("589421447974486047")
 setInterval(function() {
 channel.send(`أبي حساب فول أكسيس`);
-}, 1000)
+}, 99999)
 })
 
 client.login(process.env.BOT_TOKEN);
